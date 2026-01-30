629c84a863d7de682fdc4b0fce88f45e1a13654c
Alfie5640 <alfiewalker5640@gmail.com>
2026-01-30 09:58:33 +0000

    learned about master files

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Home Page</title>
+
+    </head>
+    <body>
+        
+        {{ $slot }}
+        
+        <x-nav-link href="/">Home</x-nav-link>
+        <x-nav-link href="/about">About</x-nav-link>
+        <x-nav-link href="/contact">Contact</x-nav-link>
+        
+    </body>
+</html>
